b9257695f87af2a79e40eb7e61a06ddc661ac1d7
Patrik <Patrikios@users.noreply.github.com>
2023-07-18 23:00:47 +0200

    Update streamlit_app.py
    bug in too many writes into snowflake on each interaction with the dashboard

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #print it out
 streamlit.dataframe(fruityvice_normalized)
 
+# do not run anything passt here till I troubleshoot
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
